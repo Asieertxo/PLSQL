fe2648c2cdd3699d956a385970040b44491d7ae1
Asieertxo <asgal.gl@gmail.com>
2022-05-27 02:11:37 +0200

    seguir

--- Strings.sql
+++ Strings.sql
@@ -8,6 +8,9 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('Minusculas: ' || LOWER(nombre));
     DBMS_OUTPUT.PUT_LINE('Iniciales mayusculas: ' || INITCAP(nombre));
     DBMS_OUTPUT.PUT_LINE('Parte del string: ' || SUBSTR(nombre, 3, 3));
-    DBMS_OUTPUT.PUT_LINE('Encuentra coincidencias: ' || INSTR(nombre, 'a'));
+    DBMS_OUTPUT.PUT_LINE('Busca la posicion de la coincidencia: ' || INSTR(nombre, 'a'));
+    DBMS_OUTPUT.PUT_LINE('corta el caracter por ambos lados: ' || TRIM('a' from nombre));
+    DBMS_OUTPUT.PUT_LINE('corta el caracter por la izquierda: ' || LTRIM(nombre, 'a'));
+    DBMS_OUTPUT.PUT_LINE('corta el caracter por la derecha: ' || RTRIM(nombre, 'a'));
 
 END;
