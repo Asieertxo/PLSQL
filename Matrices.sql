403388c633222f4a8098c669b23b04d5183ea6d4
Asieertxo <asgal.gl@gmail.com>
2022-05-27 02:13:07 +0200

    seguir

--- Matrices.sql
+++ Matrices.sql
@@ -1,18 +1,18 @@
 set serveroutput on;
 
 DECLARE    
-    numero number(2);
-    multiplo number(2) := 1;
-
-BEGIN
-    for numero in /*reverse*/ 1..10 loop
-        while multiplo <= 10 loop
-            DBMS_OUTPUT.PUT_LINE(numero || 'x' || multiplo || ' = ' || (numero*multiplo));
-            multiplo := multiplo + 1;
-        end loop;
-        DBMS_OUTPUT.PUT_LINE('-----------------------------------');
-        multiplo := 1;
+    type a_nombres is varray(5) of varchar2(20);--matriz de 5 elementos de tipo varcahr2;
+    type a_edad is varray(5) of integer;
+    nombres a_nombres;--declaramos la variable
+    edad a_edad;
+    total integer;
+    
+BEGIN  
+    nombres := a_nombres('Asier', 'Juan', 'Pedro', 'Marcos', 'Felipe');
+    edad := a_edad(20,30,22,28,25);
+    total := nombres.count;
+    for f in 1..total loop
+         DBMS_OUTPUT.PUT_LINE(nombres(f) || ' con edad ' || edad(f));
     end loop;
-
 
 END;
